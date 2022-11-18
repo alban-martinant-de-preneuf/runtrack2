e3881c41138ef06b81577b113bc4d62f4e5b5f15
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2022-11-18 13:07:57 +0100

    Ajout de vérification pour que l'utilisateur entre une largeur paire

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -11,40 +11,48 @@
     <input type="text" name="height" placeholder="hauteur">
     <input type="submit">
 </form>
-  
-<?php 
 
-$insideLine = '';
-$before = '';
+<?php
 
-for ($i = 0; $i < (int)$_GET['width'] / 2 - 1; $i++) {
-    $before = $before . ' ';
-}
+if (isset($_GET["width"]) || isset($_GET["height"])) {
 
-echo "<pre>";
-
-for ($i = 0; $i < $_GET["width"] / 2; $i++) {
-    echo $before . '/' . $insideLine . "\\<br>";
-    if ($i < $_GET["width"] / 2 - 1 ) {
-        $insideLine = $insideLine . '__';
-    }
-    $before = substr($before, 0, -1);
-}
-
-$insideSpace = '';
-
-for ($i = 0; $i < $_GET['width'] - 2; $i++) {
-    $insideSpace = $insideSpace . ' ';
-}
-
-for ( $i = 0; $i < $_GET["height"]; $i++ ) {
-    if ($i < $_GET["height"] - 1) {
-        echo '|' . $insideSpace . '|<br>';
+    if ($_GET["width"] % 2 === 0) {
+        $insideLine = '';
+        $before = '';
+    
+        for ($i = 0; $i < (int)$_GET['width'] / 2 - 1; $i++) {
+            $before = $before . ' ';
+        }
+    
+        echo "<pre>";
+    
+        for ($i = 0; $i < $_GET["width"] / 2; $i++) {
+            echo $before . '/' . $insideLine . "\\<br>";
+            if ($i < $_GET["width"] / 2 - 1) {
+                $insideLine = $insideLine . '__';
+            }
+            $before = substr($before, 0, -1);
+        }
+    
+        $insideSpace = '';
+    
+        for ($i = 0; $i < $_GET['width'] - 2; $i++) {
+            $insideSpace = $insideSpace . ' ';
+        }
+    
+        for ($i = 0; $i < $_GET["height"]; $i++) {
+            if ($i < $_GET["height"] - 1) {
+                echo '|' . $insideSpace . '|<br>';
+            } else {
+                echo '|' . $insideLine . '|<br>';
+            }
+        }
+        echo "<pre>";
     } else {
-        echo '|' . $insideLine . '|<br>';
+        echo "La largeur doit être un nombre paire.";
     }
+
+} else {
+    echo "Entrez une largeur paire et une hauteur quelconque";
 }
-
-echo "<pre>";
-
 ?>
