a8231431fa660870a1f3ba3c8502d210c4702e40
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2022-11-23 16:27:48 +0100

    version fonctionnelle

--- jour08/job05/index.php
+++ jour08/job05/index.php
@@ -2,6 +2,29 @@
 
 session_start();
 
+function checkWinner(string $playerSymbole): string {
+    $winner = "-";
+    // vérifier lignes
+    for ($i=0; $i<3; $i++) {
+        if ($_SESSION['gameBoard'][$i][0] == $playerSymbole && $_SESSION['gameBoard'][$i][1] == $playerSymbole && $_SESSION['gameBoard'][$i][2] == $playerSymbole) {
+            $winner = $playerSymbole;
+        }
+    }
+    // vérifier colonnes
+    for ($i=0; $i<3; $i++) {
+        if ($_SESSION['gameBoard'][0][$i] == $playerSymbole && $_SESSION['gameBoard'][1][$i] == $playerSymbole && $_SESSION['gameBoard'][2][$i] == $playerSymbole) {
+            $winner = $playerSymbole;
+        }
+    }
+    // vérifier diagonales
+    $diag1 = $_SESSION['gameBoard'][0][0] == $playerSymbole && $_SESSION['gameBoard'][1][1] == $playerSymbole && $_SESSION['gameBoard'][2][2] == $playerSymbole;
+    $diag2 = $_SESSION['gameBoard'][0][2] == $playerSymbole && $_SESSION['gameBoard'][1][1] == $playerSymbole && $_SESSION['gameBoard'][2][0] == $playerSymbole;
+    if ($diag1 || $diag2) {
+        $winner = $playerSymbole;
+    }
+    return $winner;
+}
+
 if (!isset($_SESSION['gameBoard'])) {
     $_SESSION['gameBoard'] = [
         ["-","-","-"],
@@ -44,6 +67,11 @@ if (isset($_GET['case'])) {
             break;
     }
     $_SESSION['turn'] += 1;
+    $winner = checkWinner($playerSymbole);
+
+    if ($winner === "X" || $winner === "O") {
+        echo 'bravo ' . $winner;
+    }
 }
 
 if (isset($_GET['reset'])) {
