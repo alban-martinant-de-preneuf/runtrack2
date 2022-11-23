0255795ba3220a00e6ff1fc291e559c2e9d780d9
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2022-11-23 11:39:04 +0100

    ajout redirection

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -5,7 +5,9 @@ session_start();
 if (!isset($_SESSION['nbvisites'])) $_SESSION['nbvisites'] = 0;
 
 if (isset($_GET['reset'])) {
-    $_SESSION['nbvisites'] = 0;
+    session_destroy();
+    header('Location: index.php');
+    exit();
 }
 
 $_SESSION['nbvisites'] += 1;
