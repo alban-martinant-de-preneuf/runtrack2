d949d33941f0dc765df400227b97a2925e165b51
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2022-11-25 11:40:18 +0100

    jour10 job08

---
+++ jour10/job08/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$mySqli = new mysqli('localhost', 'root', '', 'jour09');
+
+$request = $mySqli->query('SELECT SUM(`capacite`) AS capacite_totale FROM `salles`; ');
+
+$result = $request->fetch_array(MYSQLI_ASSOC);
+
+?>
+
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+    }
+</style>
+
+<table>
+    <thead>
+        <tr>
+            <?php foreach ($result as $key => $value) {
+                echo '<th>' . $key . '</th>';
+            } ?>
+
+        </tr>
+    </thead>
+    <tbody>
+
+        <?php
+        while ($result !== NULL) {
+            echo '<tr>';
+            foreach ($result as $value) {
+                echo '<td>' . $value . '</td>';
+            }
+            $result = $request->fetch_array(MYSQLI_ASSOC);
+            echo '</tr>';
+        }  ?>
+
+    </tbody>
+
+</table>
