4578e1bbd77a58231695c211bd5c82dbe37e59f0
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2022-11-25 11:27:26 +0100

    jour10 job06

---
+++ jour10/job06/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$mySqli = new mysqli('localhost', 'root', '', 'jour09');
+
+$request = $mySqli->query('SELECT COUNT(*) AS `Nombre d\'étudiants` FROM `etudiants`; ');
+
+$result = $request->fetch_array(MYSQLI_ASSOC);
+
+?>
+
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+    }
+</style>
+
+<table>
+    <thead>
+        <tr>
+            <?php foreach ($result as $key => $value) {
+                echo '<th>' . $key . '</th>';
+            } ?>
+
+        </tr>
+    </thead>
+    <tbody>
+
+        <?php
+        do {
+            echo '<tr>';
+            foreach ($result as $key => $value) {
+                echo '<td>' . $value . '</td>';
+            }
+            $result = $request->fetch_array(MYSQLI_ASSOC);
+            echo '</tr>';
+        } while ($result !== NULL) ?>
+
+    </tbody>
+
+</table>
